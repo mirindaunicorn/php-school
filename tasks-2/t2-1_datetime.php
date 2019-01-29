05cb879d7db3b4b8e6a8297ba98c7cca6ab2790f
mirindaunicorn <panic.ability@gmail.com>
2019-01-29 19:36:21 +0200

    tasks from lesson #2
    update .gitignore
    
    remove .idea folder
    
    update .gitignore

--- tasks-2/t2-1_datetime.php
+++ tasks-2/t2-1_datetime.php
@@ -1,4 +1,4 @@
 <?php
 declare(strict_types=1);
 
-
+echo date('l, d F, Y');
--- tasks-2/t2-3_calculating.php
+++ tasks-2/t2-3_calculating.php
@@ -1,2 +1,6 @@
 <?php
 declare(strict_types=1);
+
+$x = (sqrt((pow(4, 10))-(pow(19, 4)))*2*pow( M_PI, 2))/log10(100);
+
+echo $x;
